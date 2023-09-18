9aed3b22c0a92a8cad2b6c782d195fc72d7637c1
f0nc <f0nc@users.noreply.github.com>
2023-09-18 20:32:46 +0300

    Todo list entries are now sorted and grouped by due date

--- app/components/EntryList.js
+++ app/components/EntryList.js
@@ -23,16 +23,50 @@ export default function EntryList(props) {
         }
     }
 
-    const listEntries = entries.map(entry =>
-        <li key={entry.id}>
-            <a onClick={() => displayModal(entry)} >{entry.description}</a>
-        </li>
+    const sortedUniqueDueDates = entries
+        // Get dates
+        .map(entry => entry.dueDateTime.split('T')[0]) 
+        // Remove duplicates
+        .reduce((acc, v) => {
+            if (!acc.includes(v)) {
+                acc.push(v);
+            }
+            return acc;
+        },[])
+        // Convert to Date and sort
+        .map(dateString => new Date(dateString))
+        .sort((a, b) => a.getTime() - b.getTime());
+
+    const listEntriesByDueDate = sortedUniqueDueDates.map(dueDate => {
+            const entriesForDueDate = entries.filter(entry => {
+                const dueDateString = entry.dueDateTime.split('T')[0];
+                const entryDueDate = new Date(dueDateString);
+
+                return entryDueDate.getTime() === dueDate.getTime();
+            })
+
+            const listItems = entriesForDueDate.map(entry =>
+                <li key={entry.id}>
+                    <a onClick={() => displayModal(entry)} >{entry.description}</a>
+                </li>
+            );
+
+            const dateFormattingOptions = {day: "numeric", year: 'numeric', month: 'numeric'};
+            return (
+                <div key={dueDate}>
+                    <h5>{dueDate.toLocaleDateString(undefined, dateFormattingOptions)}</h5>
+                    <ul className="pb-3">
+                        {listItems}
+                    </ul>
+                </div>
+            )
+        }
     );
 
     return (
         <>
             <p className="fs-3">List items</p>
-            <ul>{listEntries}</ul>
+            {listEntriesByDueDate}
             <EntryDetailsModal 
                 showModal={showEntryDetailsModal} 
                 entry={entryInModal} 
